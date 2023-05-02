282ec9787fbe011ca3281f2231b4a49d7c074bf1
Sergei Saliukov <sergei_saliukov@icloud.com>
2023-05-02 23:25:10 +0200

    -

--- Sources/SafariCover/SafariButton.swift
+++ Sources/SafariCover/SafariButton.swift
@@ -13,7 +13,9 @@ public func openSafari(_ url: String, completion: @escaping () -> Void = {}) {
 
 @available(iOS 14, macOS 11, tvOS 14, watchOS 7, *)
 public struct SafariButton<Content: View>: View {
+    
     @Binding var showSafari: Bool
+    
     let url: String
     let label: Content
     
