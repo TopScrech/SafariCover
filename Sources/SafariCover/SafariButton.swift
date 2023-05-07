223ff37644f319c6cb8b0308cc9ab663a871542f
Sergei Saliukov <sergei_saliukov@icloud.com>
2023-05-07 10:19:35 +0200

    -

--- Sources/SafariCover/SafariButton.swift
+++ Sources/SafariCover/SafariButton.swift
@@ -15,27 +15,37 @@ public func openSafari(_ url: String, completion: @escaping () -> Void = {}) {
 public struct SafariButton<Content: View>: View {
     let url: String
     let label: Content
+    let content: () -> Content
     
-    public init(_ url: String, @ViewBuilder label: () -> Content) {
+    public init(
+        _ url: String,
+        @ViewBuilder label: () -> Content,
+        @ViewBuilder content: @escaping () -> Content = { EmptyView() as! Content }
+    ) {
         self.url = url
         self.label = label()
+        self.content = content
     }
     
     @State private var showSafari = false
     
     public var body: some View {
         Menu {
-            Button {
-                showSafari = true
-            } label: {
-                Label("In-app Safari", systemImage: "safari")
+            Section {
+                Button {
+                    showSafari = true
+                } label: {
+                    Label("In-app Safari", systemImage: "safari")
+                }
+                
+                Button {
+                    openSafari(url)
+                } label: {
+                    Label("Safari", systemImage: "safari.fill")
+                }
             }
             
-            Button {
-                openSafari(url)
-            } label: {
-                Label("Safari", systemImage: "safari.fill")
-            }
+            content()
         } label: {
             label
         }
